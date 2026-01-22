d71168c3deca883f8a82b5760b50eeb228c41d6c
Ilya <ilyadoledenok@yandex.ru>
2026-01-22 11:12:02 +0300

    Add files via upload

--- gom_bin.py
+++ gom_bin.py
@@ -59,6 +59,7 @@ class ReedMuller:
         return self.major_decode(W, self.__sk[0])
 
     def major_decode(self, W, S):
+        S = set(S)
         mes = np.zeros(self.__k, dtype=np.bool_)
         pos = self.__k
         for i in range(self.__r, 0, -1):
@@ -69,7 +70,14 @@ class ReedMuller:
                 cur_ind = 0
                 for j in range(2 ** (self.__m - i)):
                     cur_ind = np.where(np.prod(self.__G[np.delete(np.arange(1, self.__m + 1, dtype=int), combin[t, :]), :] == table[j].reshape(self.__m - i, 1), axis=0))[0]
-                    if not np.intersect1d(S, cur_ind).shape[0]: ######## ПОМЕНЯЯЯЯЯТЬ
+
+                    flag = 1
+                    for k in range(cur_ind.shape[0]):
+                        if cur_ind[k] in S:
+                            flag = 0
+                            break
+
+                    if flag:
                         break
                 mes[pos + t] = np.bitwise_xor.reduce(W[cur_ind])
 
@@ -91,24 +99,48 @@ class ReedMuller:
     def get_sk(self):
         return self.__sk
 
-t_sum = 0
-for i in range(100):
-    for m in range(11, 12):
-        for r in range(2, 3):
-            t_begin = perf_counter()
-            coder = ReedMuller(r, m)
-            mes = np.random.randint(0, 2, size = coder.get_k(), dtype=np.bool_)
+t_begin = perf_counter()
 
-            cip = coder.encrypt(mes)
+t_sum_enc = 0
+t_sum_dec = 0
+t_sum_prod = 0
+t_sum_sum = 0
 
-            dec = coder.decrypt(cip)
+count = 10
+r = 1
+m = 18
 
-            res = np.prod(mes == dec)
-            t_end = perf_counter() - t_begin
-            t_sum += t_end
-            print(r, m, res, t_end)
-            if not res:
-                print("Error")
-                exit(1)
-print("All right")
-print(t_sum / 100)
+for i in range(count):
+    coder = ReedMuller(r, m)
+
+    mes1 = np.random.randint(0, 2, size=coder.get_k(), dtype=np.bool_)
+    mes2 = np.random.randint(0, 2, size=coder.get_k(), dtype=np.bool_)
+
+    t = perf_counter()
+    cip1 = coder.encrypt(mes1)
+    cip2 = coder.encrypt(mes2)
+    t_sum_enc += perf_counter() - t
+
+    t = perf_counter()
+    dec1 = coder.decrypt(cip1)
+    dec2 = coder.decrypt(cip2)
+    t_sum_dec += perf_counter() - t
+
+    t = perf_counter()
+    cip3 = cip1 ^ cip2
+    t_sum_sum += perf_counter() - t
+
+    t = perf_counter()
+    cip4 = cip1 & cip2
+    t_sum_prod += perf_counter() - t
+
+    #res = np.prod((mes1 ^ mes2) == coder.decrypt(cip3)) + np.prod((mes1 * mes2) == coder.decrypt(cip4))
+    print(perf_counter() - t_begin)
+    #if res != 2:
+    #    print("Error")
+    #    exit(1)
+
+print(f"Time encryption: {t_sum_enc / (2 * count):.10f}")
+print(f"Time decryption: {t_sum_dec / (2 * count):.10f}")
+print(f"Time sum: {t_sum_sum / count:.10f}")
+print(f"Time prod: {t_sum_prod / count:.10f}")
